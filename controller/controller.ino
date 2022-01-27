b84805eb34df269b904a4ee81f938b833732c65d
Jayden Elliott <jdelliott2003@gmail.com>
2022-01-27 15:54:59 -0500

    added second co2 sensor

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,39 +1,49 @@
 #include <SoftwareSerial.h>
 
 // CO2 SENSOR SETUP
-SoftwareSerial sensor(12, 13);  // RX, TX pins on Ardunio
+// RX, TX pins on Ardunio
+SoftwareSerial cozir(12, 13);
+SoftwareSerial sprintir(10, 11);
 
 // RELAY BOARD SETUP
 const int pump = 6;
 const int valve = 7;
 
 // GLOBALS
-int co2;
+int cozir_co2;
 int temp;
 int hum;
+int sprintir_co2;
 
 void setup() {
     Serial.begin(9600);
-    sensor.begin(9600);  // Start serial communications with sensor
-    sensor.println("M 4164");  // set mode for co2, temp, and humidity outputs
-    sensor.println("K 2");  // set polling mode
+
+    cozir.listen();
+    cozir.begin(9600);  // Start serial comms with cozir sensor
+    cozir.println("M 4164");  // set mode for co2, temp, and humidity outputs
+    cozir.println("K 2");  // set polling mode
+
+    sprintir.listen();
+    sprintir.begin(9600);  // Start serial comms with sprintir sensor
+    sprintir.println("M 4");  // set mode for filtered co2 output
+    sprintir.println("K 2");  // set polling mode
 
     pinMode(pump, OUTPUT);
     pinMode(valve, OUTPUT);
 
-    co2 = 0;
+    cozir_co2 = 0;
     temp = 0;
     hum = 0;
 }
 
 void loop() {
-    refresh();
-    Serial.println("CO2: " + String(co2));
-    Serial.println("Temp: " + String(temp));
-    Serial.println("Hum: " + String(hum));
-    Serial.println();
+    refresh_cozir();
+    refresh_sprintir();
+    Serial.println("COZIR READING\nCO2: " + String(cozir_co2) +
+                   "\nTemp: " + String(temp) + "\nHum: " + String(hum) + "\n");
+    Serial.println("SPRINTIR READING\nCO2: " + String(sprintir_co2) + "\n");
 
-    if (co2 >= 1900) {
+    if (cozir_co2 >= 1900) {
         digitalWrite(valve, HIGH);
     } else {
         digitalWrite(valve, LOW);
@@ -42,16 +52,29 @@ void loop() {
     delay(1000);
 }
 
-void refresh() {
-    sensor.println("Q");
-    String reading = sensor.readStringUntil('\n');
+void refresh_cozir() {
+    cozir.listen();
+    
+    cozir.println("Q");
+    String reading = cozir.readStringUntil('\n');
+    String co2_in = reading.substring(19, 24);
+    cozir_co2 = co2_in.toInt();
 
-    String co2_in = reading.substring(18, 24);
-    co2 = co2_in.toInt();
-
-    String temp_in = reading.substring(10, 16);
+    String temp_in = reading.substring(11, 16);
     temp = (temp_in.toInt() - 1000) / 10;
 
-    String hum_in = reading.substring(2, 8);
+    String hum_in = reading.substring(3, 8);
     hum = hum_in.toInt() / 10;
+}
+
+    void refresh_sprintir() {
+    sprintir.listen();
+    
+    sprintir.println(".");
+    String reading = sprintir.readStringUntil('\n');
+    int multiplier = reading.substring(3,8).toInt();
+    
+    sprintir.println("Z");
+    reading = sprintir.readStringUntil('\n');
+    sprintir_co2 = multiplier * reading.substring(3, 8).toInt();
 }
