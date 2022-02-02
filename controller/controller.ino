a15b2a9ef71d415677da7c325f0cc920613d2c3b
Jayden Elliott <jdelliott2003@gmail.com>
2022-02-01 19:16:56 -0500

    sensor class in sensor.h

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,49 +1,31 @@
 #include <SoftwareSerial.h>
+#include "sensor.h"
 
 // CO2 SENSOR SETUP
-// RX, TX pins on Ardunio
-SoftwareSerial cozir(12, 13);
-SoftwareSerial sprintir(10, 11);
+cozir cozir1(6, 7);
+cozir cozir2(8, 9);
+cozir cozir3(10, 11);
+sprintir sprintir1(12, 13);
 
-// RELAY BOARD SETUP
-const int pump = 6;
-const int valve = 7;
-
-// GLOBALS
-int cozir_co2;
-int temp;
-int hum;
-int sprintir_co2;
+// RELAY BOARD PINS
+const int pump = 3;
+const int valve = 4;
 
 void setup() {
     Serial.begin(9600);
 
-    cozir.listen();
-    cozir.begin(9600);  // Start serial comms with cozir sensor
-    cozir.println("M 4164");  // set mode for co2, temp, and humidity outputs
-    cozir.println("K 2");  // set polling mode
-
-    sprintir.listen();
-    sprintir.begin(9600);  // Start serial comms with sprintir sensor
-    sprintir.println("M 4");  // set mode for filtered co2 output
-    sprintir.println("K 2");  // set polling mode
-
     pinMode(pump, OUTPUT);
     pinMode(valve, OUTPUT);
-
-    cozir_co2 = 0;
-    temp = 0;
-    hum = 0;
 }
 
 void loop() {
-    refresh_cozir();
-    refresh_sprintir();
-    Serial.println("COZIR READING\nCO2: " + String(cozir_co2) +
-                   "\nTemp: " + String(temp) + "\nHum: " + String(hum) + "\n");
-    Serial.println("SPRINTIR READING\nCO2: " + String(sprintir_co2) + "\n");
+    refresh_all();
+    Serial.println("COZIR READING\nCO2: " + String(cozir3.get_co2()) +
+                   "\nTemp: " + String(cozir3.get_temp()) +
+                   "\nHum: " + String(cozir3.get_hum()) + "\n");
+    Serial.println("SPRINTIR READING\nCO2: " + String(sprintir1.get_co2()) + "\n");
 
-    if (cozir_co2 >= 1900) {
+    if (cozir3.get_co2() >= 1900) {
         digitalWrite(valve, HIGH);
     } else {
         digitalWrite(valve, LOW);
@@ -52,29 +34,9 @@ void loop() {
     delay(1000);
 }
 
-void refresh_cozir() {
-    cozir.listen();
-    
-    cozir.println("Q");
-    String reading = cozir.readStringUntil('\n');
-    String co2_in = reading.substring(19, 24);
-    cozir_co2 = co2_in.toInt();
-
-    String temp_in = reading.substring(11, 16);
-    temp = (temp_in.toInt() - 1000) / 10;
-
-    String hum_in = reading.substring(3, 8);
-    hum = hum_in.toInt() / 10;
-}
-
-    void refresh_sprintir() {
-    sprintir.listen();
-    
-    sprintir.println(".");
-    String reading = sprintir.readStringUntil('\n');
-    int multiplier = reading.substring(3,8).toInt();
-    
-    sprintir.println("Z");
-    reading = sprintir.readStringUntil('\n');
-    sprintir_co2 = multiplier * reading.substring(3, 8).toInt();
+void refresh_all() {
+    cozir1.refresh();
+    cozir2.refresh();
+    cozir3.refresh();
+    sprintir1.refresh();
 }
---
+++ controller/sensor.h
@@ -0,0 +1,76 @@
+#include <SoftwareSerial.h>
+
+class cozir {
+   public:
+    // REQUIRES: type is "COZIR" or "SPRINTIR"
+    cozir(int rx, int tx) : sensor(rx, tx), co2(0), temp(0), hum(0) {
+        sensor.listen();
+        sensor.begin(9600);  // Start serial comms with cozir3 sensor
+        sensor.println("M 4164");  // set mode for co2, temp, and humidity outputs
+        sensor.println("K 2");  // set polling mode
+    }
+
+    int get_co2() const {
+        return co2;
+    }
+
+    int get_temp() const {
+        return temp;
+    }
+
+    int get_hum() const {
+        return hum;
+    }
+
+    void refresh() {
+        sensor.listen();
+
+        sensor.println("Q");
+        String reading = sensor.readStringUntil('\n');
+        String co2_in = reading.substring(19, 24);
+        co2 = co2_in.toInt();
+
+        String temp_in = reading.substring(11, 16);
+        temp = (temp_in.toInt() - 1000) / 10;
+
+        String hum_in = reading.substring(3, 8);
+        hum = hum_in.toInt() / 10;
+    }
+
+   private:
+    SoftwareSerial sensor;
+    int co2;
+    int temp;
+    int hum;
+};
+
+class sprintir {
+   public:
+    // REQUIRES: type is "COZIR" or "SPRINTIR"
+    sprintir(int rx, int tx) : sensor(rx, tx), co2(0) {
+        sensor.listen();
+        sensor.begin(9600);  // Start serial comms with sprintir sensor
+        sensor.println("M 4");  // set mode for filtered co2 output
+        sensor.println("K 2");  // set polling mode
+    }
+
+    int get_co2() const {
+        return co2;
+    }
+
+    void refresh() {
+        sensor.listen();
+
+        sensor.println(".");
+        String reading = sensor.readStringUntil('\n');
+        int multiplier = reading.substring(3, 8).toInt();
+
+        sensor.println("Z");
+        reading = sensor.readStringUntil('\n');
+        co2 = multiplier * reading.substring(3, 8).toInt();
+    }
+
+   private:
+    SoftwareSerial sensor;
+    int co2;
+};
